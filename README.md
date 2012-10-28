1378dbf9214be8990bc5bab106a435b6b85d93c4
Antoine Kougblenou <antoine.kougblenou@gmail.com>
2012-10-28 23:34:28 +0100

    first initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Antoine Kougblenou](http://antoinekougblenou.com)
+
+Welcome to antoinekougblenou.com built by Antoine Kougblenou and powered by [HTML5 Boilerplate](http://html5boilerplate.com)
---
+++ old/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<html>
+<!-- Everything else goes in here -->
+	<head>
+		<style type="text/css"> 
+			body{
+				background-color: #000;
+			}
+			.container{ 
+				margin:170px auto;
+				width:940px;
+			}
+			img{
+				margin: 0px auto;
+			}
+			img{
+				display: block;
+			}
+		</style>
+		<link rel="icon"  type="image/png"  href="favicon.png" />
+		<title>Home</title>
+	</head>
+	<body>
+			<!-- using div instead of section for the moment as the definition is not clear yet -->
+		<div class="container">
+			<img src="holding-page.png" alt="Site Coming Soon" />
+		</div>
+
+	</body>
+<!-- Ending with the closing tag: -->
+</html>
